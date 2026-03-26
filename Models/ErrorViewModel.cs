65e4681534faef3b828a1bf47d75983e519dd86b
Gregorio Rojas <gregorio_rojas@usmp.pe>
2026-03-26 00:37:42 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace trabajo_de_SF2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
